f47b0a128787fc5ae8fef1e0c00027241777d6b6
victor-kiss <victorkiss10@gmail.com>
2024-01-04 10:38:37 -0300

    main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,148 @@
-import Image from 'next/image'
+"use client";
 
+
+
+
+import Image from "next/image";
+import Link from "next/link";
+import Experience from "./models/experience";
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <Experience/>
+      <section
+        className="h-screen p-6 flex justify-center items-center align-center content-center  w-auto"
+        id="discovery"
+      >
+        <div className="w-[40em]">
+          <h1 className="font-bold text-gray-100 sm:text-3xl text-2xl p-4">
+            Bitten by a radioactive spider, Peter Parker's arachnid abilities
+            give him amazing powers he uses to help others, while his personal
+            life continues to offer plenty of obstacles. With amazing
+            spider-like abilities, teenage science whiz Peter Parker fights
+            crime and dreams of becoming an Avenger as Spider-Man.
+          </h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      </section>
+      <section className="h-screen flex flex-col justify-center  items-center  w-client items-center flex-wrap p-6">
+        <h2 className="font-bold text-gray-300 text-4xl text-center mb-4">
+         FRIST  APPEARANCE
+        </h2>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/images/frist_comic.jpg"
+          width={300}
+          height={500}
+          className="my-4"
+          alt="AMAZING FANTASY (1962) #15 Thumb"
+          priority={true}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <span className="text-gray-400 font-bold text-lg">
+          AMAZING FANTASY (1962) #15
+        </span>
+      </section>
+      <section className="sm:h-screen">
+        <div className="sm:h-screen">
+          <h2 className="font-bold text-gray-100 text-4xl text-center">
+            LAST GAMES:
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="flex justify-center align-center w-client items-center flex-wrap">
+            <div className="bg-[#202028] sm:w-[30em] rounded-lg sm:h-[40em] my-8 shadow-xl mx-8 relative">
+              <Image
+                src="/images/spider_man2.png"
+                width={300}
+                height={300}
+                className="w-full h-[15em] rounded-lg"
+                priority={true}
+                alt="spider-man 2 thumb"
+              />
+              <div className="w-full p-4" alt="spider-man 2">
+                <span className="font-bold text-gray-300 text-xl my-2">
+                  Marvel's Spider-Man 2
+                </span>
+                <span className="text-gray-300 my-2 block">released: 2023</span>
+                <p className=" text-gray-400 mt-10">
+                  Spider-Men, Peter Parker and Miles Morales, return for an
+                  exciting new adventure in the critically acclaimed
+                  Marvel&apos;s Spider-Man. Swing, jump and utilize the new Web
+                  Wings to travel across Marvel&apos;s New York.
+                </p>
+              </div>
+              <div className="sm:absolute bottom-0 p-4 bg-[#161616] w-full">
+                <span className="block text-gray-200 text-center mb-4 text-sm">
+                  Avaliable on
+                </span>
+                <div className="w-full flex justify-center">
+                  <Link href="https://www.playstation.com/en-ca/games/marvels-spider-man-2/" rel="noreferrer" target="_blank">
+                    <Image
+                      src="/images/playstation.svg"
+                      width={24}
+                      height={24}
+                      alt="playstation display icon"
+                      className="mx-2"
+                      priority={true}
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="bg-[#202028] sm:w-[30em] rounded-lg sm:h-[40em] my-8 shadow-xl mx-8 relative">
+              <Image
+                src="/images/spider_man_remaster.jpg"
+                width={300}
+                height={300}
+                className="w-full h-[15em] rounded-lg"
+                alt="spider-man remastered"
+                priority={true}
+              />
+              <div className="w-full p-4">
+                <span className="font-bold text-gray-300 text-xl my-2">
+                  Marvel&apos;s Spider-Man Remastered
+                </span>
+                <span className="text-gray-300 my-2 block">released: 2022</span>
+                <p className=" text-gray-400 mt-10">
+                  This isn&apos;t the Spider-Man you&apos;ve met or ever seen
+                  before. In Marvel&apos;s Spider-Man Remastered, we meet an
+                  experienced Peter Parker who&apos;s more masterful at fighting
+                  big crime in New York City. At the same time, he&apos;s
+                  struggling to balance his chaotic personal life and career
+                  while the fate of Marvel&apos;s New York rests upon his
+                  shoulders.
+                </p>
+              </div>
+              <div className="sm:absolute bottom-0 p-4 bg-[#161616] w-full ">
+                <span className="block text-gray-200 text-center mb-4 text-sm">
+                  Avaliable on
+                </span>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                <div className="bg-[#161616] w-full flex justify-center">
+                  <Link href="https://www.playstation.com/en-ca/games/marvels-spider-man-remastered/" rel="noreferrer" target="_blank">
+                    <Image
+                      src="/images/playstation.svg"
+                      width={24}
+                      height={24}
+                      alt="playstation display icon"
+                      className="mx-2"
+                      priority={true}
+                    />
+                  </Link>
+                
+                  <Link href="https://store.steampowered.com/app/1817070/Marvels_SpiderMan_Remastered/" rel="noreferrer" target="_blank">
+                    <Image
+                      src="/images/pc.svg"
+                      width={24}
+                      height={24}
+                      alt="pc display icon"
+                      className="mx-2"
+                      priority={true}
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
